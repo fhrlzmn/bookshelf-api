de509492c024ecd9633b0fc390d52652bc86b832
fhrlzmn <fhrlzmn26@gmail.com>
2023-03-23 10:20:13 +0700

    Create HTTP Server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-console.log('works');
+const Hapi = require('@hapi/hapi');
+
+const PORT = 9000;
+const HOST = 'localhost';
+
+async function init() {
+  const server = Hapi.server({
+    host: HOST,
+    port: PORT,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+}
+
+init();
